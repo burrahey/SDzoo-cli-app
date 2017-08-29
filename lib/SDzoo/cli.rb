9d96bf515d61cf775577aa9e732d0d904eae53d4
Archana Burra <burra.archana@gmail.com>
2017-08-28 21:56:01 -0400

    added some code into methods

--- lib/SDzoo/cli.rb
+++ lib/SDzoo/cli.rb
@@ -3,15 +3,7 @@ class SDzoo::CLI
   def call
     puts "Welcome to the San Diego Zoo CLI app!"
     puts "To get started, what type of animals would you like to learn more about?"
-    puts  <<~HEREDOC
-      1. Mammals
-      2. Reptiles
-      3. Birds
-      4. Amphibians
-      5. Insects
-      6. Fish
-    HEREDOC
-    puts "You can type a number or name."
+    main_menu
     input = gets.strip
 
     while input != "exit"
@@ -28,90 +20,23 @@ class SDzoo::CLI
         HEREDOC
         puts "You can type a number or name."
       when "1" || "mammals"
-        create_animals("mammals")
-        SDzoo::MAMMALS.display_all
-
-        puts "What animal would you like to find out more about? Enter a number."
-        more = gets.strip.to_i
-        if more < SDzoo::MAMMALS.all.length
-          animal = SDzoo::MAMMALS.all[more - 1]
-          add_attributes_to_animals(animal)
-          animal.display_all_attributes
-        end
-
+        mammals
       when "2" || "reptiles"
-        create_animals("reptiles")
-        SDzoo::REPTILES.display_all
-
-        puts "What animal would you like to find out more about? Enter a number."
-        more = gets.strip.to_i
-        if more < SDzoo::REPTILES.all.length
-          animal = SDzoo::REPTILES.all[more - 1]
-          add_attributes_to_animals(animal)
-          animal.display_all_attributes
-        end
-
+        reptiles
       when "3" || "birds"
-        create_animals("birds")
-        SDzoo::BIRDS.display_all
-
-        puts "What animal would you like to find out more about? Enter a number."
-        more = gets.strip.to_i
-        if more < SDzoo::BIRDS.all.length
-          animal = SDzoo::BIRDS.all[more - 1]
-          add_attributes_to_animals(animal)
-          animal.display_all_attributes
-        end
-
+        birds
       when "4" || "amphibians"
-        create_animals("amphibians")
-        SDzoo::AMPHIBIANS.display_all
-
-        puts "What animal would you like to find out more about? Enter a number."
-        more = gets.strip.to_i
-        if more < SDzoo::AMPHIBIANS.all.length
-          animal = SDzoo::AMPHIBIANS.all[more - 1]
-          add_attributes_to_animals(animal)
-          animal.display_all_attributes
-        end
-
+        amphibians
       when "5" || "insects"
-        create_animals("arthropods")
-        SDzoo::ARTHROPODS.display_all
-        puts "What animal would you like to find out more about? Enter a number."
-        more = gets.strip.to_i
-        if more < SDzoo::ARTHROPODS.all.length
-          animal = SDzoo::ARTHROPODS.all[more - 1]
-          add_attributes_to_animals(animal)
-          animal.display_all_attributes
-        end
-
+        arthropods
       when "6" || "fish"
-        create_animals("fish")
-        SDzoo::FISH.display_all
-
-        puts "What animal would you like to find out more about? Enter a number."
-        more = gets.strip.to_i
-        if more < SDzoo::MAMMALS.all.length
-          animal = SDzoo::MAMMALS.all[more - 1]
-          add_attributes_to_animals(animal)
-          animal.display_all_attributes
-        end
-
+        fish
       when 'exit'
         break
       end
       sleep(2)
       puts "What kind of animal would you like to learn about next?"
-      puts  <<~HEREDOC
-        1. Mammals
-        2. Reptiles
-        3. Birds
-        4. Amphibians
-        5. Insects
-        6. Fish
-      HEREDOC
-      puts "You can type a number or name, or 'exit'"
+      main_menu
       input = gets.strip
     end
 
@@ -125,6 +50,95 @@ class SDzoo::CLI
   def add_attributes_to_animals(animal)
     animal_hash = SDzoo::SCRAPER.add_attributes_to_animal(animal.url)
     animal.add_attributes_to_animal(animal_hash)
+  end
+
+  def main_menu
+    puts  <<~HEREDOC
+      1. Mammals
+      2. Reptiles
+      3. Birds
+      4. Amphibians
+      5. Insects
+      6. Fish
+    HEREDOC
+    puts "You can type a number or name, or 'exit'"
+  end
+
+  def mammals
+    create_animals("mammals")
+    SDzoo::MAMMALS.display_all
+
+    puts "What animal would you like to find out more about? Enter a number."
+    more = gets.strip.to_i
+    if more < SDzoo::MAMMALS.all.length
+      animal = SDzoo::MAMMALS.all[more - 1]
+      add_attributes_to_animals(animal)
+      animal.display_all_attributes
+    end
+  end
+
+  def birds
+    create_animals("birds")
+    SDzoo::BIRDS.display_all
+
+    puts "What animal would you like to find out more about? Enter a number."
+    more = gets.strip.to_i
+    if more < SDzoo::BIRDS.all.length
+      animal = SDzoo::BIRDS.all[more - 1]
+      add_attributes_to_animals(animal)
+      animal.display_all_attributes
+    end
+  end
+
+  def reptiles
+    create_animals("reptiles")
+    SDzoo::REPTILES.display_all
+
+    puts "What animal would you like to find out more about? Enter a number."
+    more = gets.strip.to_i
+    if more < SDzoo::REPTILES.all.length
+      animal = SDzoo::REPTILES.all[more - 1]
+      add_attributes_to_animals(animal)
+      animal.display_all_attributes
+    end
+  end
+
+  def fish
+    create_animals("fish")
+    SDzoo::FISH.display_all
+
+    puts "What animal would you like to find out more about? Enter a number."
+    more = gets.strip.to_i
+    if more < SDzoo::MAMMALS.all.length
+      animal = SDzoo::MAMMALS.all[more - 1]
+      add_attributes_to_animals(animal)
+      animal.display_all_attributes
+    end
+  end
+
+  def arthropods
+    create_animals("arthropods")
+    SDzoo::ARTHROPODS.display_all
+    puts "What animal would you like to find out more about? Enter a number."
+    more = gets.strip.to_i
+    if more < SDzoo::ARTHROPODS.all.length
+      animal = SDzoo::ARTHROPODS.all[more - 1]
+      add_attributes_to_animals(animal)
+      animal.display_all_attributes
+    end
+  end
+
+  def amphibians
+    create_animals("amphibians")
+    SDzoo::AMPHIBIANS.display_all
+
+    puts "What animal would you like to find out more about? Enter a number."
+    more = gets.strip.to_i
+    if more < SDzoo::AMPHIBIANS.all.length
+      animal = SDzoo::AMPHIBIANS.all[more - 1]
+      add_attributes_to_animals(animal)
+      animal.display_all_attributes
+    end
   end
 
   def bye
